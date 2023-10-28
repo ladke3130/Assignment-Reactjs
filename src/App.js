3d9b2438b96b88633ce689e95e6ed027f112cb52
nikita ladke <117341099+ladke3130@users.noreply.github.com>
2023-10-28 19:10:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f0f0f0;
+`;
+
+const LoginForm = styled.form`
+  border: 1px solid #ccc;
+  padding: 30px;
+  border-radius: 8px;
+  background-color: #fff;
+  box-shadow: 0px 0px 15px 0px rgba(0,0,0,0.1);
+  max-width: 400px;
+  width: 100%;
+`;
+
+const Title = styled.h2`
+  text-align: center;
+`;
+
+const FormGroup = styled.div`
+  margin-bottom: 20px;
+`;
+
+const Label = styled.label`
+  display: block;
+  margin-bottom: 5px;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 10px;
+  border-radius: 4px;
+  border: 1px solid #ccc;
+`;
+
+const SubmitButton = styled.button`
+  width: 100%;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  background-color: #007bff;
+  color: white;
+  cursor: pointer;
+`;
 
 function App() {
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <LoginForm onSubmit={handleFormSubmit}>
+        <Title>Log In</Title>
+        <FormGroup>
+          <Label htmlFor="username">Username</Label>
+          <Input type="text" id="username" placeholder="Enter your username" />
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="password">Password</Label>
+          <Input type="password" id="password" placeholder="Enter your password" />
+        </FormGroup>
+        <SubmitButton type="submit">Login</SubmitButton>
+      </LoginForm>
+    </Wrapper>
   );
 }
 
